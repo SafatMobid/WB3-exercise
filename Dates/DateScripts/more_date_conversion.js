f31ac42d5beb71235ebe784b27150cf3bae7f86e
Safat Mobid <safatmobid@gmail.com>
2022-10-26 15:11:38 -0400

    completed more_date_conversion exercise

---
+++ Dates/DateScripts/more_date_conversion.js
@@ -0,0 +1,8 @@
+let myBirthdayDate = new Date("1996/11/11");
+console.log("My birthday is: " + myBirthdayDate. toLocaleString());
+
+let motherBirthdayDate = new Date("January 1  1962");
+console.log("My mother birthday is: " + motherBirthdayDate.toLocaleString());
+
+let fatherBirthdayDate = new Date(1954 ,01 , 01 );
+console.log("My father birthday is: " + fatherBirthdayDate.toLocaleString());
