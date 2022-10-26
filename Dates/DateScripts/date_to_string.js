2809b1e5baa7fccd8eee483f7a74c4a37651eb10
Safat Mobid <safatmobid@gmail.com>
2022-10-26 14:36:12 -0400

    Finished date_to_string exercise

---
+++ Dates/DateScripts/date_to_string.js
@@ -0,0 +1,7 @@
+let date = new Date ();
+
+console.log("Current time: " + date) 
+console.log("toString(): " + date.toString())
+console.log("toDateString(): " + date.toDateString()) 
+console.log("toUTCString(): " + date.toUTCString()) 
+console.log("toLocaleString(): " + date.toLocaleString())
